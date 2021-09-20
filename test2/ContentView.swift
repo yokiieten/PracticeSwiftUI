2e5dc7cf101eee394c0530ed33e26d88ceced43a
Super Production <yokten10@gmail.com>
2021-09-20 23:01:44 +0700

    Initial Commit

---
+++ test2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  test2
+//
+//  Created by Yok on 20/9/2564 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ test2/test2App.swift
@@ -0,0 +1,17 @@
+//
+//  test2App.swift
+//  test2
+//
+//  Created by Yok on 20/9/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct test2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
